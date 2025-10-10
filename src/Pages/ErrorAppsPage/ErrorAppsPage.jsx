1d21f319016126098682d940e0034c2eb6309fcd
Asif-Orbit <asifsweuestc@gmail.com>
2025-10-11 00:20:11 +0800

    Error Page minor bug fix

--- src/Pages/ErrorAppsPage/ErrorAppsPage.jsx
+++ src/Pages/ErrorAppsPage/ErrorAppsPage.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import errorPage from '../../assets/App-Error.png'
 import { Link } from 'react-router';
 const ErrorAppsPage = () => {
-   const handleBack=()=> {
-    window.location.reload()
-   }
     return (
         <div>
             <div className='flex flex-col text-center items-center mt-10'>
@@ -12,7 +9,7 @@ const ErrorAppsPage = () => {
                 <h1 className='font-semibold text-5xl text-black mt-5 mb-5'>OPPS!! APP NOT FOUND</h1>
                 <p className='text-[#627382]'>The App you are requesting is not found on our system.  please try another apps</p>
 
-                <Link to="/Apps"><button onClick={handleBack} className='btn mt-5 bg-gradient-to-r font-semibold text-[#FFFFFF] from-[#632EE3] to-[#9F62F2]'>Go Back</button></Link>
+                <Link to="/Apps"><button className='btn mt-5 bg-gradient-to-r font-semibold text-[#FFFFFF] from-[#632EE3] to-[#9F62F2]'>Go Back</button></Link>
 
             </div>
         </div>
--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import Navbar from '../../Components/Navbar/Navbar';
 import errorPage from '../../assets/error-404.png'
 import { Link } from 'react-router';
+import Footer from '../../Components/Footer/Footer';
 
 const ErrorPage = () => {
-    const handleBack =()=> {
-        window.location.reload()
-    }
+
     return (
         <div>
             <Navbar></Navbar>
@@ -15,9 +14,10 @@ const ErrorPage = () => {
                 <h1 className='font-semibold text-5xl text-black mt-5 mb-5'>Oops, page not found!</h1>
                 <p className='text-[#627382]'>The page you are looking for is not available.</p>
 
-                <Link to="/"><button onClick={handleBack} className='btn mt-5 bg-gradient-to-r font-semibold text-[#FFFFFF] from-[#632EE3] to-[#9F62F2]'>Go Back</button></Link>
+                <Link to="/"><button  className='btn mt-5 bg-gradient-to-r font-semibold text-[#FFFFFF] from-[#632EE3] to-[#9F62F2]'>Go Back</button></Link>
 
             </div>
+            <Footer></Footer>
 
         </div>
     );
